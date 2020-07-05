062012702ba7cce9acbf8bea49554e974af74a29
PaulAditya <adityapaul1412@gmail.com>
2020-07-05 12:56:03 +0530

    user autologin onauthchange

--- lib/src/homePage.dart
+++ lib/src/homePage.dart
@@ -3,17 +3,7 @@ import 'package:NYOB/src/loginPage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-enum AuthStatus {
-  NOT_DETERMINED,
-  NOT_LOGGED_IN,
-  LOGGED_IN,
-}
 
-FirebaseAuth _auth = FirebaseAuth.instance;
-
-Future<FirebaseUser> getUser() async {
-  return await _auth.currentUser();
-}
 
 class HomePage extends StatefulWidget {
   @override
@@ -21,23 +11,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  AuthStatus authStatus;
-
-  @override
-  void initState() {
-    super.initState();
-    getUser().then((user) {
-      print(user);
-      if (user.uid != null) {
-        authStatus = AuthStatus.LOGGED_IN;
-      }else if(user.uid == null){
-        authStatus = AuthStatus.NOT_LOGGED_IN;
-      }else{
-        authStatus = AuthStatus.NOT_DETERMINED;
-      }
-    });
-  }
-
+  
   Widget buildWaitingScreen() {
     return Scaffold(
       body: Container(
@@ -49,18 +23,18 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    switch (authStatus) {
-      case AuthStatus.NOT_DETERMINED:
-        return buildWaitingScreen();
-        break;
-      case AuthStatus.NOT_LOGGED_IN:
-        return LoginPage();
-        break;
-      case AuthStatus.LOGGED_IN:
-        return ChatPage();
-        break;
-      default:
-        return buildWaitingScreen();
-    }
+
+    
+    return StreamBuilder<FirebaseUser>(
+      stream: FirebaseAuth.instance.onAuthStateChanged,
+      builder: (BuildContext context, snapshot) {
+        FirebaseUser user = snapshot.data;
+        if (snapshot.hasData) {
+          return ChatPage(user: user,);
+        } else {
+          return LoginPage();
+        }
+      },
+    );
   }
 }
